a3127f1d0af6b0e3754bf39e9eb27b976ca79bb5
cs222wa <cs222wa@studen.lnu.se>
2014-10-16 18:01:13 +0200

    Finished Test-programs.

--- Digital Alarmclock/AlarmClock.cs
+++ Digital Alarmclock/AlarmClock.cs
@@ -8,29 +8,76 @@ namespace Digital_Alarmclock
 {
     class AlarmClock
     {
+
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
         //Egenskap, som kapslar in det privata fältet _alarmHour. set-metoden måste validera att värdet som 
         //ska tilldelas _alarmHour är i det slutna intervallet mellan 0 och 23. Är värdet inte i intervallet ska ett 
         //undantag av typen ArgumentException kastas. 
-        public int AlarmHour {get; set;}
-        int _alarmHour;
+        public int AlarmHour
+        {
+            get { return _alarmHour; }
+            set
+            {
+                if (0 > value || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                _alarmHour = value;
+            }
+        }
 
         //Egenskap, som kapslar in det privata fältet _alarmMinute. set-metoden måste validera att värdet som 
         //ska tilldelas _alarmMinute är i det slutna intervallet mellan 0 och 59. Är värdet inte i intervallet ska 
         //ett undantag av typen ArgumentException kastas.
-        public int AlarmMinute {get; set;}
-        int _alarmMinute;
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                if (0 > value || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                _alarmMinute = value;
+            }
+        }
 
         //Egenskap, som kapslar in det privata fältet _hour. set-metoden måste validera att värdet som ska 
         //tilldelas _hour är i det slutna intervallet mellan 0 och 23. Är värdet inte i intervallet ska ett undantag 
         //av typen ArgumentException kastas.
-        int Hour {get; set;}
-        int _hour;
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                if (0 > value || value > 23)
+                {
+                    throw new ArgumentException();
+                }
+                _hour = value;
+            }
+        }
+
 
         //Egenskap, som kapslar in det privata fältet _minute. set-metoden måste validera att värdet som ska 
         //tilldelas _minute är i det slutna intervallet mellan 0 och 59. Är värdet inte i intervallet ska ett undantag 
         //av typen ArgumentException kastas.
-        int Minute {get; set;}
-        int _minute;
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                if (0 > value || value > 59)
+                {
+                    throw new ArgumentException();
+                }
+                _minute = value;
+            }
+        }
 
         // Konstruktorerna.
         // Konstruktorerna, som är tre till antalet, ska se till att ett AlarmClock-objekt blir korrekt initierat. Det 
@@ -50,7 +97,8 @@ namespace Digital_Alarmclock
         //konstruktor i klassen som har fyra parametrar.
         public AlarmClock(int hour, int minute)
         {
-
+            Hour = hour;
+            Minute = minute;
         }
 
         //Med konstruktorn AlarmClock(int hour, int minute , int alarmHour, int alarmMinute)
@@ -58,9 +106,12 @@ namespace Digital_Alarmclock
         //anger. Detta är den enda av konstruktorerna som får innehålla kod som leder till att fält i klassen 
         //tilldelas värden.
         public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+            : this(hour, minute)
         {
-
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
         }
+
         //Publik metod som anropas för att få klockan att gå en minut. Om den nya tiden överensstämmer med 
         //alarmtiden ska metoden returnera true, annars false. Inga utskrifter till konsolfönstret får göras av 
         //metoden.
@@ -69,17 +120,73 @@ namespace Digital_Alarmclock
         //1. Värdet för timmarna måste vara i det slutna intervallet mellan 0 och 23.
         public bool TickTock()
         {
-            return false;
+            if (Minute < 59)
+            {
+                Minute++;
+            }
+            else
+            {
+                Minute = 0;
+                if (Hour < 23)
+                {
+                    Hour++;
+                }
+                else
+                {
+                    Hour = 0;
+                }
+            }
+
+            if (AlarmHour == Hour && AlarmMinute == Minute)
+            {
+                return true;
+            }
+            return false;                                               //Om inte kraven uppfylls returnerar metoden värdet false.
         }
 
         //Publik metod som har som uppgift att returnera en sträng representerande värdet av en instans av 
         //klassen AlarmClock. Inga utskrifter till konsolfönstret får göras av metoden.
         //Är timmen ett ental ska timmen presenteras utan att inledas med 0, medan då minuterna är ental ska de 
         //inledas med 0. Är klocka fem över elva på kvällen ska tiden presenteras som 23:05. Är tiden åtta 
-        //minuter över sju på morgonen ska tiden presenteras som 7:08. 
+        //minuter över sju på morgonen ska tiden presenteras som 7:08. 
+
         public string ToString()
         {
-            
+            //string currentTime = DateTime.Now.ToString("HH:mm");
+            return String.Format("{0}:{1:d2} ({2}:{3:d2})", Hour, Minute, AlarmHour, AlarmMinute);
         }
     }
 }
+
+//NOTES
+//Console.WriteLine("Minutes?");                Konsolen begär minuter utav användaren.
+//    string minutes = Console.ReadLine();      Läser in minuter
+//    int mins = int.Parse(minutes);            String mins ändras till int minutes.
+//    
+//        // Write line.                        Vid varje minut skrivs ett meddelande ut.
+//        Console.WriteLine(new string('X', 30));
+
+// Larmet piper 10 gånger
+////for (int i = 0; i < 10; i++)
+//    {
+//        Console.Beep();
+//    }
+
+
+//while (current_time != alarm_time) {    när nuvarande tid är samma som larmtiden - sound alarm.
+//    sleep(60);
+//}
+//sound_alarm();
+
+
+//Let's say that I have a method in my application, and I want this method to be called 
+//every hour on the hour (e.g. at 7:00 PM, 8:00 PM, 9:00 PM etc.). 
+//I could create a Timer and set its Interval to 3600000, but eventually this would drift out of sync with the system clock. 
+//Or I could use a while() loop with Thread.Sleep(n) to periodically check the system time 
+//and call the method when the desired time is reached, 
+
+
+//If (Format(DateTime.Now, "hh:mm") = Format(AlarmTime.Value, "hh:mm"))
+
+//DateTime.Now.ToString("HH:mm:ss tt");
+//this gives it to you as a string.
--- Digital Alarmclock/Program.cs
+++ Digital Alarmclock/Program.cs
@@ -8,29 +8,187 @@ namespace Digital_Alarmclock
 {
     class Program
     {
+        private static string horizontalLine;
+
+        public string HorizontalLine
+        {
+            get { return horizontalLine; }
+            set
+            {
+                horizontalLine = "════════════════════════════════════════";
+            }
+        }
+
         //Metoden ska instansiera objekt av klassen AlarmClock och testa konstruktorerna, egenskaperna och 
         //metoderna.
         static void Main(string[] args)
         {
+            AlarmClock ac = new AlarmClock(10, 15, 10, 30);
+
+            AlarmClock clock = new AlarmClock();
+            ViewTestHeader("Test 1. Test av standardkonstruktorn.");
+            string time = clock.ToString();
+            Console.WriteLine(time);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            AlarmClock secondClock = new AlarmClock(9, 42);
+            ViewTestHeader("Test 2. Test av konstruktorn med två parametrar (9, 42).");
+            string secondTime = secondClock.ToString();
+            Console.Write(secondTime);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            AlarmClock thirdClock = new AlarmClock(13, 24, 7, 35);
+            ViewTestHeader("Test 3. Test av konstruktorn med fyra parametrar (13, 24, 7, 35).");
+            string thirdTime = thirdClock.ToString();
+            Console.WriteLine(thirdTime);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+            Console.WriteLine();
+
+            AlarmClock fourthClock = new AlarmClock(23, 58, 0, 11);
+            ViewTestHeader("Test 4. Låt larmet gå efter 13 minuter.");
+            for (int i = 0; i < 15; i++)
+            {
+                if (fourthClock.TickTock() == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string fourthTime = fourthClock.ToString();
+                    Console.Write(fourthTime);
+                    Console.WriteLine("BEEP! BEEP! BEEP!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    string fourthTime = fourthClock.ToString();
+                    Console.WriteLine(fourthTime);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+
+            AlarmClock fifthClock = new AlarmClock(6, 12, 6, 15);
+            ViewTestHeader("Test 5. Låt larmet gå efter 6 minuter.");
+            for (int i = 0; i < 6; i++)
+            {
+                if (fifthClock.TickTock() == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string fifthTime = fifthClock.ToString();
+                    Console.Write(fifthTime);
+                    Console.WriteLine("BEEP! BEEP! BEEP!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    string fifthTime = fifthClock.ToString();
+                    Console.WriteLine(fifthTime);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+
+            //AlarmClock sixthClock = new AlarmClock(44, 60, 25, 81);
+            //ViewTestHeader("Test 6. Test av egenskaperna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.");
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (sixthClock.TickTock() == true)
+            //    {
+            //        Console.BackgroundColor = ConsoleColor.Blue;
+            //        Console.ForegroundColor = ConsoleColor.White;
+            //        string sixthTime = sixthClock.ToString();
+            //        Console.Write(sixthTime);
+            //        Console.WriteLine("BEEP! BEEP! BEEP!");
+            //        Console.ResetColor();
+            //    }
+            //    else
+            //    {
+            //        string sixthTime = sixthClock.ToString();
+            //        Console.WriteLine(sixthTime);
+            //    }
+            //}
+
+            //AlarmClock seventhClock = new AlarmClock(44, 60, 25, 81);
+            //ViewTestHeader("Test 7. Test av konstruktorer så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.");
+            //string seventhTime = seventhClock.ToString();
+            //Console.WriteLine(seventhTime);
+            //Console.WriteLine();
+            //Console.ForegroundColor = ConsoleColor.Blue;
+            //Console.WriteLine(Program.horizontalLine);
+            //Console.ResetColor();
+
+            //Man kan självklart dela upp detta i återanvändningsbara funktioner, men prova först 
+            //att få koden att fungera utan detta. Därefter se om du kan göra koden mer 
+            //återanvändningsbar med att göra en funktion som gör mycket jobbet för dig.
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+            Console.WriteLine("AlarmClock");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Program.horizontalLine);
+            Console.ResetColor();
+            Console.WriteLine();
+            Program.Run(ac, 20);
         }
 
-        //Metoden ska instansiera objekt av klassen AlarmClock och testa konstruktorerna, egenskaperna och 
-        //metoderna.
-        public void Run(AlarmClock ac, int minutes)
+        //Privat statisk metod som har två parametrar.
+        //Den första parametern är en referens till AlarmClock-objekt.
+        // Den andra parametern är antalet minuter som AlarmClock-objektet ska gå (vilket lämpligen görs genom att låta ett
+        // AlarmClock-objekt göra upprepade anrop av metoden TickTock()).
+        private static void Run(AlarmClock ac, int minutes)
         {
-                
+            for (int i = 0; i < minutes; i++)
+            {
+                if (ac.TickTock() == true)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string acTime = ac.ToString();
+                    Console.Write(acTime);
+                    Console.WriteLine("BEEP! BEEP! BEEP!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    string acTime = ac.ToString();
+                    Console.WriteLine(acTime);
+                }
+            }
         }
 
         //Privat statisk metoden som tar ett felmeddelande som argument och presenterar det.
-        public void ViewErrorMessage(string message)
+        private void ViewErrorMessage(string message)
         {
-
+            Console.WriteLine(message);
         }
 
         //Privat statisk metod som tar en sträng som argument och presenterar strängen.
-        public void ViewTestHeader(string header)
+        private static void ViewTestHeader(string header)
         {
-
+            Console.WriteLine(header);
         }
     }
 }
+
+
+//NOTES
+//Console.WriteLine("Ange den aktuella timmen: ");
+//fourthClock.Hour = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ange den aktuella minuten: ");
+//fourthClock.Minute = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ange timme för larmet: ");
+//fourthClock.AlarmHour = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ange minuten för larmet: ");
+//fourthClock.AlarmMinute = int.Parse(Console.ReadLine());
